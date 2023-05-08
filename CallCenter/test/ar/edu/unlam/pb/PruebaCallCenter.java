bf38c44b139de6c278be908ed084015257a8f449
Axel Daniel <axeelleguero@gmail.com>
2023-05-07 22:13:56 -0300

    Integrador CallCenter con JUnit

---
+++ CallCenter/test/ar/edu/unlam/pb/PruebaCallCenter.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaCallCenter {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
