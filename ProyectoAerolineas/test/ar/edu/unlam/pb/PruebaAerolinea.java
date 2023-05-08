26168dd8c1001194a2762b4d2415fa517fa4514e
Axel Daniel <axeelleguero@gmail.com>
2023-05-07 21:49:49 -0300

    Proyecto aerolinea iniciado con JUnit

---
+++ ProyectoAerolineas/test/ar/edu/unlam/pb/PruebaAerolinea.java
@@ -0,0 +1,12 @@
+package ar.edu.unlam.pb;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaAerolinea {
+
+	@Test
+	public void Test() {
+		fail("Not yet implemented");
+	}
+}
